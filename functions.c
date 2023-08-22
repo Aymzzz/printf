d048fc17673738f4751b8e4ba4769537922b9b44
codingbottt21 <138158974+codingbottt21@users.noreply.github.com>
2023-08-22 20:17:34 +0200

    Update functions.c

--- functions.c
+++ functions.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 
 /************************* PRINT CHAR *************************/
@@ -186,4 +187,31 @@ int print_binary(va_list types, char buffer[],
 	}
 	return (count);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
